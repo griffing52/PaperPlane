813d31b5fefa0fb92b9fb80cba6bcbe9656db47b
Bolun Thompson <me@bolun.dev>
2025-12-02 20:49:46 -0800

    fix: allow front-end to do request

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,12 +6,17 @@ import { Request, Response, NextFunction } from "express";
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    credentials: true
+  }
+));
 app.use(express.json());
 app.use(router);
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   logger.error(err, 'Internal server error');
-  
+
   res.status(500).json({
     error: 'Internal server error',
   });
